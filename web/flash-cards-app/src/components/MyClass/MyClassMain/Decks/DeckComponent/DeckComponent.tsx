23a20285092b5d375aa172015644cf75eab3843d
iagosergiop93 <iagosergiop93@gmail.com>
2020-09-25 15:26:11 -0400

    Added Login Page

--- web/flash-cards-app/src/components/MyClass/MyClassMain/Decks/DeckComponent/DeckComponent.tsx
+++ web/flash-cards-app/src/components/MyClass/MyClassMain/Decks/DeckComponent/DeckComponent.tsx
@@ -21,19 +21,19 @@ export class DeckComponent extends React.Component<DeckProps> {
         return (
             <div className="deck-row">
                 <div className="deck-checkbox deck-row-item" onClick={() => this.clickedCheckbox()}>
-                    { !this.props.deck.checkbox ? undefined: <img src={process.env.PUBLIC_URL + '/assets/icons/checkmark-circle.png'} /> }
+                    { !this.props.deck.checkbox ? undefined: <img alt="checkbox-icon" src={process.env.PUBLIC_URL + '/assets/icons/checkmark-circle.png'} /> }
                 </div>
                 <span className="deck-row-item">{this.props.deck.name}</span>
                 <div className="option-deck deck-row-item">
-                    <img src={process.env.PUBLIC_URL + '/assets/icons/shuffle.png'}/>
+                    <img alt="shuffle-option" src={process.env.PUBLIC_URL + '/assets/icons/shuffle.png'}/>
                     <span>Reorder</span>
                 </div>
                 <div className="option-deck deck-row-item">
-                    <img src={process.env.PUBLIC_URL + '/assets/icons/eye.png'}/>
+                    <img alt="view-option" src={process.env.PUBLIC_URL + '/assets/icons/eye.png'}/>
                     <span>View Deck</span>
                 </div>
                 <div className="option-deck deck-row-item">
-                    <img src={process.env.PUBLIC_URL + '/assets/icons/pencil.png'}/>
+                    <img alt="edit-option" src={process.env.PUBLIC_URL + '/assets/icons/pencil.png'}/>
                     <span>Edit Deck</span>
                 </div>
                 <button className="study-btn">Study</button>
--- web/flash-cards-app/src/pages/Login/Login.tsx
+++ web/flash-cards-app/src/pages/Login/Login.tsx
@@ -1,10 +1,51 @@
+import { Form, Input, Button } from 'antd';
+import './Login.css';
 import React from 'react';
 
-export class Login extends React.Component {
+export class Login extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            username: "",
+            passwd: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+    }
+
+    handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        
+        this.setState(() => ({ [name]: value }));
+
+        console.log(name, value);
+    }
+
+    handleSubmit() {
+        console.log('username', this.state.username);
+        console.log('passwd', this.state.passwd);
+    }
 
     render() {
         return (
-            "Login page"
+            <div className="login-container">
+                <span className="title">Sign in</span>
+                <Form layout="vertical">
+                    <Form.Item label="Username">
+                        <Input placeholder="username" name="username" onChange={this.handleChange} />
+                    </Form.Item>
+                    <Form.Item label="Password">
+                        <Input placeholder="password" name="passwd" onChange={this.handleChange} />
+                    </Form.Item>
+                    <Form.Item className="button-container">
+                        <Button type="primary" size="middle" onClick={this.handleSubmit} block>Submit</Button>
+                    </Form.Item>
+                </Form>
+            </div>
         );
     }
 
