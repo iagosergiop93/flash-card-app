d17362105c814d169f7d7518b6325391286f8749
iagosergiop93 <iagosergiop93@gmail.com>
2020-09-26 14:44:32 -0400

    Added Signup page

--- web/flash-cards-app/src/App.tsx
+++ web/flash-cards-app/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard/Dashboard';
 import { Login } from './pages/Login/Login';
 import { Header } from './components/Header/Header';
+import { Signup } from './pages/Signup/Signup';
 
 function App() {
   return (
@@ -11,8 +12,12 @@ function App() {
       <Header title="Flash-Cards"/>
       <Router>
         
-        <Route exact path="/">
+        <Route exact path="/signin">
           <Login />
+        </Route>
+
+        <Route exact path="/signup">
+          <Signup />
         </Route>
 
         <Route exact path="/dashboard" 
